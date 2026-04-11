9da0d83843de10c1c5cad9ed33ee9e42b8a654e3
saadazghour <saad.azghour@gmail.com>
2026-04-11 21:37:03 +0100

    feat(WarmUp/MovingOn): Update collision detection logic

--- WarmUp/MovingOn/task/src/collision.cpp
+++ WarmUp/MovingOn/task/src/collision.cpp
@@ -3,9 +3,11 @@
 #include "game.hpp"
 
 float distance(Point2D a, Point2D b) {
+    float dx = a.x - b.x;
+    float dy = a.y - b.y;
     return sqrt(dx * dx + dy * dy);
 }
 
 bool collision(Circle circle1, Circle circle2) {
-    return false;
+    return distance(circle1.center, circle2.center) < circle1.radius + circle2.radius;
 }
