d0b2e724d19dc2e1f7d76b4c2870f08177440ffa
saadazghour <saad.azghour@gmail.com>
2026-04-16 01:01:15 +0100

    feat(WarmUp/MovingOn): update game loop logic
    - Updated loop.cpp
    - Improved main simulation/game loop

--- WarmUp/MovingOn/task/src/loop.cpp
+++ WarmUp/MovingOn/task/src/loop.cpp
@@ -1,5 +1,9 @@
 #include "game.hpp"
 
 void collisionLoop(Circle player, Circle consumable[], bool consumed[], int size) {
-    return;
+    for (int i = 0; i < size; ++i) {
+        if (!consumed[i]) {
+            consumed[i] = collision(player, consumable[i]);
+        }
+    }
 }
